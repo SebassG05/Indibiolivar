12cfb9f60953dea85f37ec7deca31f00c439a67f
SebassG05 <sgandiagutierrez@adaits.es>
2025-10-10 08:54:08 +0200

    commit cambio correo

--- server/earth_engine_service/app.py
+++ server/earth_engine_service/app.py
@@ -27,7 +27,7 @@ CORS(app, supports_credentials=True, origins=[
 # ria = RIA() # Commented out
 
 # Initialize Earth Engine with service account
-service_account = 'soil-values-predictor@soil-values-predictor.iam.gserviceaccount.com'
+service_account = 'evenor-tech@soil-values-predictor.iam.gserviceaccount.com'
 credentials_path = os.path.join(os.path.dirname(__file__), 'soil-values-predictor-94b681b7d4af.json')
 credentials = ee.ServiceAccountCredentials(service_account, credentials_path)
 ee.Initialize(credentials)
